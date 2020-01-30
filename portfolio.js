cb1662e8f6bfe76f1cd0b625d0dbf06aea5fc1f4
KelseyvV <59088506+KelseyvV@users.noreply.github.com>
2020-01-30 17:09:04 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+
+var menu;
+var navigatie;
+
+/* selecteren van html elementen: .menu en .navigation */
+menu = document.querySelector('.menu');
+navigatie = document.querySelector('.navigatie');
+
+function hamburgerMenu() {
+    menu.classList.toggle('active');
+    navigatie.classList.toggle('active');
+}
+
+/* door te klikken op het menu wordt de functie hamburgerMenu uitgevoerd */
+menu.addEventListener('click', hamburgerMenu);
